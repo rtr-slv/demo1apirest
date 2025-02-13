f8f77c68d74abcaa4adf3cdba17dfbe6ed168b12
Arturo Silva <asilvac@pucesm.edu.ec>
2025-02-12 20:41:31 -0500

    Demo1APIREST: FINALIZADO

---
+++ src/main/java/ec/edu/puce/demo1apirest/Demo1apirestApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.puce.demo1apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1apirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1apirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/puce/demo1apirest/Demo1apirestApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.puce.demo1apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1apirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
